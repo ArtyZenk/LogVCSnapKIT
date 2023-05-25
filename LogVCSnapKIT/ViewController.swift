47e0303be5290d228f234ff2b966e7aa6c51d636
Artyom  Guzenko <artyomka13@gmail.com>
2023-05-25 14:40:22 +0300

    delete default comments.

--- LogVCSnapKIT/ViewController.swift
+++ LogVCSnapKIT/ViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+     
     }
 
 
